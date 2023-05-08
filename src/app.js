7c7af2f310f07f67af6d1094e9a97fef2fe06e8a
ShailendraVishwakarma <110275464+ShailendraVishwakarma@users.noreply.github.com>
2023-05-08 13:39:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,35 +1,27 @@
-// Import required modules
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./models/user');
 
-// Create Express app
 const app = express();
+const port = 3000;
 
-// Set up database connection using Mongoose
-mongoose.connect('mongodb://localhost/social-media-app', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB!'))
-.catch(err => console.error('Error connecting to MongoDB', err));
+mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true });
 
-// Define app routes
 app.get('/', async (req, res) => {
   try {
-    const namePrefix = req.query.name || '';
-    const regexPattern = new RegExp(`^${namePrefix}`, 'i'); // 'i' flag for case-insensitive match
-    const usersCount = await User.countDocuments({ name: regexPattern });
-    res.status(200).send({ count: usersCount });
+    const prefixName = req.query.name || '';
+    const regex = new RegExp(`^${prefixName}`, 'i');
+    const count = await User.countDocuments({ name: regex });
+    res.json({ count });
   } catch (err) {
     console.error(err);
-    res.status(500).send({ error: 'Internal server error' });
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-// Start server
-app.listen(3000, () => {
-  console.log('Server started on port 3000!');
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
+
 
 module.exports = app;
