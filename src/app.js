017a6ecaf48ebb8ad91542733353d5f3425cefb3
ShailendraVishwakarma <110275464+ShailendraVishwakarma@users.noreply.github.com>
2023-05-08 16:25:46 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,52 @@
+
+// const express = require('express');
+// const app = express();
+// const mongoose = require('mongoose');
+// const users   =require("../models/user.js");
+
+// //Router Middlewares
+// app.use(express.json());
+
+// //Type of query (Hint)
+
+// /*
+
+// 1. / --> this means we need to consider all users
+// 2. /?name=swa --> Will return count of all the user name that have prefix swa. We will (Swaraj Jain, Swarak agrawal, etc). 
+// 3. /?name= -->this means we need to consider all users
+
+// */
+
+
+// // Complete this Route which will return the count of Number of Prefixmatch for the name in the query/
+
+// app.get("/",async function(req,res){
+
+//     var count = 0;
+
+//     //Write you code here
+//     //update count variable
+
+//     res.send(JSON.stringify(count));
+
+// });
+
+// module.exports = app;
+
+
 const express = require('express');
-const mongoose = require('mongoose');
-const User = require('./models/user');
-
 const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+const users = require("../models/user.js");
 
-mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true });
+// Router Middlewares
+app.use(express.json());
 
-app.get('/', async (req, res) => {
-  try {
-    const prefixName = req.query.name || '';
-    const regex = new RegExp(`^${prefixName}`, 'i');
-    const count = await User.countDocuments({ name: regex });
-    res.json({ count });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+// Route to get the count of users whose name starts with the given prefix
+app.get("/", async function(req, res) {
+  let namePrefix = req.query.name || "";
+  let count = await users.countDocuments({ name: { $regex: "^" + namePrefix, $options: "i" } });
+  res.send(JSON.stringify(count));
 });
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
-
 
 module.exports = app;
